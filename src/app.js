155fdabbb182a27dca86488ee76236781dba22ce
Sidney <sidney.croset@hotmail.com>
2022-06-21 19:44:39 +0200

    deploy

--- src/app.js
+++ src/app.js
@@ -29,7 +29,7 @@ app.use(
 app.use(function (req, res, next) {
   res.header(
     "Access-Control-Allow-Origin",
-    "https://portfolio-sidney.netlify.app/"
+    "https://db-portfolio-sidney.herokuapp.com/"
   );
   res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
   res.header("Access-Control-Allow-Headers", "Content-Type");
