378bab0f19c64fc39dcebdddd1d0cc3bba1cc2fa
Sidney <sidney.croset@hotmail.com>
2022-06-21 19:52:49 +0200

    deploy

--- src/app.js
+++ src/app.js
@@ -19,7 +19,12 @@ const cookieParser = require("cookie-parser");
 // Je configure CORS pour autoriser uniquement le front à communiquer avec mon API
 // Permet d'avoir accés aux cookies envoyé dans les requetes
 app.use(cookieParser());
-app.use(cors());
+app.use(
+  cors({
+    credentials: true,
+    origin: process.env.CLIENT_ORIGIN,
+  })
+);
 
 // J'indique l'accés à mon dossier public ou se trouve mes assets
 app.use("/public", express.static("public"));
